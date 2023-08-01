dc389e7d3aa2ab9abdb848d23e48b95f31cea2fa
Dosunmu <oluwamuyiwadosunmu@gmail.com>
2023-08-01 14:31:36 +0100

    Basic CRUD implemented

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -35,6 +35,7 @@ export class CoffeesController {
   @Post()
   @HttpCode(HttpStatus.GONE)
   create(@Body() createCoffeeDto: CreateCoffeeDto) {
+    console.log(createCoffeeDto instanceof CreateCoffeeDto);
     return this.coffeeService.create(createCoffeeDto);
   }
 
--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,14 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+  /**These code below helps to keep our vlidation in check */
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
