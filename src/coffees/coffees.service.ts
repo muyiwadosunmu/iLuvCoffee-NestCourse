44cf8ab3e73f91d1ea1fc3ddf32133ab01592c51
Dosunmu <oluwamuyiwadosunmu@gmail.com>
2023-08-03 16:23:50 +0100

    Using Cascading inserts and updates

--- src/coffees/coffees.service.ts
+++ src/coffees/coffees.service.ts
@@ -9,12 +9,15 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { CreateCoffeeDto } from './dto/create-coffee.dto/create-coffee.dto';
 import { UpdateCoffeeDto } from './dto/update-coffee.dto/update-coffee.dto';
+import { Flavor } from './entities/flavor.entity/flavor.entity';
 
 @Injectable()
 export class CoffeesService {
   constructor(
     @InjectRepository(Coffee)
     private readonly coffeeRepository: Repository<Coffee>,
+    @InjectRepository(Flavor)
+    private readonly flavorRepository: Repository<Flavor>,
   ) {}
 
   async findAll() {
@@ -39,14 +42,28 @@ export class CoffeesService {
   }
 
   async create(createCoffeeDto: CreateCoffeeDto) {
-    const coffee = this.coffeeRepository.create(createCoffeeDto);
+    const flavors = await Promise.all(
+      createCoffeeDto.flavors.map((name) => this.preLoadFlavorByName(name)),
+    );
+
+    const coffee = this.coffeeRepository.create({
+      ...createCoffeeDto,
+      flavors,
+    });
     return this.coffeeRepository.save(coffee);
   }
 
   async update(id: string, updateCoffeeDto: UpdateCoffeeDto) {
+    const flavors =
+      updateCoffeeDto.flavors &&
+      (await Promise.all(
+        updateCoffeeDto.flavors.map((name) => this.preLoadFlavorByName(name)),
+      ));
+
     const coffee = await this.coffeeRepository.preload({
       id: +id,
       ...updateCoffeeDto,
+      flavors,
     });
 
     if (!coffee) {
@@ -58,5 +75,16 @@ export class CoffeesService {
   async remove(id: string) {
     const coffee = await this.findOne(id);
     return this.coffeeRepository.remove(coffee);
+  }
+
+  private async preLoadFlavorByName(name: string): Promise<Flavor> {
+    const existingFlavor = await this.flavorRepository
+      .createQueryBuilder('flavor')
+      .where('flavor.name = name', { name })
+      .getOne();
+    if (existingFlavor) {
+      return existingFlavor;
+    }
+    return this.flavorRepository.create({ name });
   }
 }
