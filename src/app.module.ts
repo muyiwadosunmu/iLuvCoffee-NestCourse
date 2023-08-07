163823b393abda2e4aaecab8c9312fb218cf12dd
Dosunmu <oluwamuyiwadosunmu@gmail.com>
2023-08-07 11:43:28 +0100

    We set up our .env file, we installed the nestjs config package for this

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,17 +8,19 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { type } from 'os';
 import { CoffeeRatingModule } from './coffee-rating/coffee-rating.module';
 import { DatabseModule } from './databse/databse.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     CoffeesModule,
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'pass123',
-      database: 'postgres',
+      host: process.env.DATABASE_HOST,
+      port: Number(process.env.DATABASE_PORT),
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
       autoLoadEntities: true,
       synchronize: true, //Disable this on production
     }),
